1ca5fbc7d713b9e9a5ff03f3aeefe49f3c6bc6bf
Shannon Yang <myang.shannon1122@gmail.com>
2019-11-26 23:32:06 +0800

    新增文件夹

---
+++ day3/14石头剪刀布/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  14石头剪刀布
+//
+//  Created by Shannon MYang on 2019/11/26.
+//  Copyright © 2019 Shannon MYang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
