5047e1e3298773b8653164823837b0a8eec2c263
Yang Xiukun <myang.shannon1122@gmail.com>
2019-11-29 14:42:10 +0800

    增加代码和注释；

--- day2/day2/main.c
+++ day2/day2/main.c
@@ -11,5 +11,6 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    system("pause");  //这句话的添加作用是，提示用户按下任意键继续操作，也就是为了阻塞，防止窗口消失；
     return 0;
 }
